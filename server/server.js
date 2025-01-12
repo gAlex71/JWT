3822da49a263d9ef3df8e9d7f95065c4869f8c59
gAlex71 <alexvladimirovich71@mail.ru>
2025-01-12 23:39:58 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const jsonServer = require('json-server');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Create a JSON Server instance
+const jsonServerInstance = jsonServer.create();
+// Specify the path to the db.json file
+jsonServerInstance.use(jsonServer.defaults());
+jsonServerInstance.use(jsonServer.router('db.json'));
+
+const start = () => {
+	try {
+		app.listen(PORT, () => console.log(`Start on PORT-${PORT}`));
+
+		// Connect the JSON Server to your Express backend
+		// app.use('/api', jsonServerInstance);
+
+		// Define a route to write a token to the db.json file
+		// app.post('/api/token', (req, res) => {
+		// 	const token = req.body.token;
+		// 	jsonServerInstance.db.get('token').push(token);
+		// 	jsonServerInstance.db.write();
+		// 	res.json({ message: 'Token written successfully' });
+		// });
+
+		// Define a route to get the token from the db.json file
+		// app.get('/api/token', (req, res) => {
+		// 	const token = jsonServerInstance.db.get('token')[0];
+		// 	if (token) {
+		// 		res.json({ token });
+		// 	} else {
+		// 		res.status(404).json({ error: 'Token not found' });
+		// 	}
+		// });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
